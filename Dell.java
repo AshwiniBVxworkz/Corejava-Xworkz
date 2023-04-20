d86c2f1c6a97c554d0b18cda6819fd921391125c
AshwiniBVxworkz <ashwinibv.xworkz@gmail.com>
2023-04-20 12:26:11 +0530

    adding java files

---
+++ Dell.java
@@ -0,0 +1,17 @@
+class Dell{
+public static void main(String lap[]){
+String brand = "Dell";
+String series="Inspiron";
+String colour="Silver";
+String ram="8 GB";
+String cou="Core i5";
+int price=69900;
+System.out.println("Dell Inspiron");
+System.out.println("Brand :"+brand);
+System.out.println("Series  :"+series);
+System.out.println("Colour : "+colour);
+System.out.println("RAM Memory Installed :"+ram);
+System.out.println("CPU Model :"+cou);
+System.out.println("Price: "+price "Rs");
+}
+}
