f533fcc77a9261bde556a598550a7e93f1cdce92
AshwiniBVxworkz <ashwinibv.xworkz@gmail.com>
2023-04-24 23:55:19 +0530

    array files

---
+++ Pub.java
@@ -0,0 +1,9 @@
+class Pub{
+	public static void main(String pub[]){
+		String pubNames[]={"What's in a Name","Toit Brewpub","Gilly's 104 Bar","Pecos Stones","District 6","The Biere Club","Hoppipola","The Noon Wine","Vapour pub","Doffpub","Dhamaka By Oat Soda","SkyDEck","Hammered Pub","Boozy Griffin","Levels Pub"};
+		System.out.println("Pubs");
+		for(int i=0;i<pubNames.length;i++){
+			System.out.println(pubNames[i]);
+			}
+}
+}
