9f7fb6d978e550e547f895809a1682a459ddd7b6
AshwiniBVxworkz <ashwinibv.xworkz@gmail.com>
2023-05-17 13:40:44 +0530

    scanner related files

--- facebook-login/src/com/xworkz/facebookapp/FacebookLogin.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.facebookapp;
-
-public class FacebookLogin {
-    public static void main(String login[]){
-        Facebook.login(9678787654l , "sd45gf78");
-        Facebook.login("joy456@gmail.com" , "sd45gf78");
-    }
-}
--- hospital/src/com/xworkz/hospitalapp/PatientTester.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.hospitalapp;
-
-public class PatientTester {
-    public static void main(String[] args){
-        Patient patient=new Patient(1,"Ram",23);
-        System.out.println(patient.patientId+" "+patient.name+" "+ patient.age);
-    }
-}
